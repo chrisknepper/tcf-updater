478886f9160ac1a4b2235f8821cdb47f08963695
Chris Knepper <chris82thekid@gmail.com>
2021-04-15 22:34:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { writeFileSync } from "fs";
+
+import { basicAuth } from "./auth.js";
+
+const app = express();
+
+const customBodyAuth = basicAuth({
+  users: { admin: "derp" },
+  challenge: true,
+  unauthorizedResponse: getUnauthorizedResponse,
+});
+
+// Parse URL-encoded bodies
+app.use(express.urlencoded());
+// Parse JSON bodies
+app.use(express.json());
+
+app.use("/public", express.static("public"));
+
+app.use("/assets", customBodyAuth, express.static("ui/assets"));
+app.get("/", customBodyAuth, express.static("ui"));
+app.post("/update", customBodyAuth, function (req, res) {
+  const { day, date, video } = req.body;
+  const updateObject = {
+    day,
+    date,
+    video,
+  };
+  writeFileSync("./public/tcf-data.json", JSON.stringify(updateObject));
+  res.status(200).send("Site updated");
+});
+
+app.listen(8080, function () {
+  console.log("Listening!");
+});
+
+function getUnauthorizedResponse(req) {
+  return req.auth
+    ? "Credentials " + req.auth.user + ":" + req.auth.password + " rejected"
+    : "No credentials provided";
+}
---
+++ ui/assets/update-helper.js
@@ -0,0 +1,4 @@
+// TODO:
+// Submit form this way?
+// Populate with current values?
+
